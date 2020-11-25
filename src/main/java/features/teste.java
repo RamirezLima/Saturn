9d05572b3e5989519f7f4c8c88a2884e57346f1f
Ramirez Lima <ramirezsoareslima@outlook.com>
2020-11-25 20:53:30 -0300

    All Project 1.0

---
+++ src/main/java/features/teste.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package features;
+
+/**
+ *
+ * @author ramir
+ */
+public class teste {
+    public static void main(String[] args) {
+        Ouvir.ouvirYoutube("a-ha", "take 0n me");
+        Ouvir.ouvirVagalume("a-ha", "take on me");
+        Ouvir.ouvirDeezer("a-ha", "take on me");
+        Ouvir.ouvirSpotify("a-ha", "take on me");
+    }
+    
+}
